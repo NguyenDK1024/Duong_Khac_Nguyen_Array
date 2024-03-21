7eb7af5f55823474b6b02ed09e3c5cf28534f365
NguyenDK1024 <duongkhacnguyen.nth@gmail.com>
2024-03-21 20:24:37 +0700

    Updated 2024-03-21

--- Program.cs
+++ Program.cs
@@ -1,71 +1,191 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Duong_Khac_Nguyen_Array
 {
     internal class Program
     {
+        //Ham tim so lan lap lai
+        static int CountDuplicates(int[] arr)
+        {
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            return count;
+        }
+        // Ham tim so duy nhat
+        static bool Co_Duplicate(int[] arr, int index)
+        {
+            for (int i = 0; i < index; i++)
+            {
+                if (arr[i] == arr[index])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
         static void Main(string[] args)
         {
-
             int s = 0;
             int Max = 0;
 
-            Console.Write("Nhap vao so luong phan tu n =");
+            Console.Write("Nhap vao so luong phan tu n = ");
             int n = int.Parse(Console.ReadLine());
             int[] A = new int[n];
 
             for (int i = 0; i < A.Length; i++)
             {
-             Console.Write("nhap gia tri cho phan tu A[{0}] = ", i);
-             A[i] = int.Parse(Console.ReadLine());
+                Console.Write("Nhap gia tri cho phan tu A[{0}] = ", i);
+                A[i] = int.Parse(Console.ReadLine());
             }
 
-            //Tinh tong
+
+            //// Bài 1
             //for (int i = 0; i < A.Length; i++)
             //{
-            //    s = s + A[i];
+            //    Console.Write(A[i] + "    ");
             //}
-            //Console.WriteLine("Tong phan tu la s = {0}",s);
 
-            //tim so lon nhat
-            for (int i = 0;i < A.Length; i++)
-            {
-                if (Max < A[i]) Max= A[i];
-            }
-            Console.WriteLine("So lon nha la {0}", Max);
-            //sap xep
-            for (int i = 0; i < A.Length-1; i++)
-            {
-                int count = 0;
-                for (int j = 0; j < A.Length - 1; j++)
-                {
-                    int Temp = 0;
-                   
-                    if (A[j] > A[j + 1])
-                    {
-                        count++;
-                        Temp = A[j];
-                        A[j] = A[j + 1];
-                        A[j + 1] = Temp;
-                    }
-                    Console.WriteLine("A[{0}] = {1}",j,A[j]);
-                    if (j == A.Length - 2)
-                    {
-                        Console.WriteLine("A[{0}] = {1}", n-1, A[n-1]);
-                        Console.WriteLine();
-                    }                                      
+            //// Bài 2
+            //Array.Reverse(A);
+            //Console.WriteLine();
+            //for (int j = 0; j < A.Length; j++)
+            //{
+            //    Console.Write(A[j] + "    ");
+            //}
 
-                }
-                if (i == A.Length - 2 && count == 0) { break; }
-            }
-            for (int i = 0; i < A.Length; i++) { Console.WriteLine(A[i]); }
-            ////////////////////////////////
-            ////////////////////////////////
-                Console.ReadKey();
+            // Bài 3
+            //int duplicateCount = CountDuplicates(A);
+            //Console.WriteLine("So luong phan tu giong nhau trong mang: " + duplicateCount);
+
+            //Bài 4
+            //Console.WriteLine("Cac phan tu duy nhat trong mang:");
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    bool Co_Duplicate = false;
+            //    for (int j = 0; j < A.Length; j++)
+            //    {
+            //        if (i != j && A[i] == A[j])
+            //        {
+            //            Co_Duplicate = true;
+            //            break;
+            //        }
+            //    }
+
+            //    if (!Co_Duplicate)
+            //    {
+            //        Console.WriteLine(A[i]);
+            //    }
+            //}
+
+            //Bài 5
+
+            //// dem so phan tu chan va le
+            //int chanCount = 0;
+            //int leCount = 0;
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    if (A[i] % 2 == 0)
+            //    {
+            //        chanCount++;
+            //    }
+            //    else
+            //    {
+            //        leCount++;
+            //    }
+            //}
+
+            //// khoi tao mang chan va le
+            //int[] chan = new int[chanCount];
+            //int[] le = new int[leCount];
+
+            //// chia phan tu mang chan va le
+            //int chanIndex = 0;
+            //int leIndex = 0;
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    if (A[i] % 2 == 0)
+            //    {
+            //        chan[chanIndex] = A[i];
+            //        chanIndex++;
+            //    }
+            //    else
+            //    {
+            //        le[leIndex] = A[i];
+            //        leIndex++;
+            //    }
+            //}
+
+            //// in mang chan
+            //Console.WriteLine("Mang chan:");
+            //for (int i = 0; i < chan.Length; i++)
+            //{
+            //    Console.WriteLine(chan[i]);
+            //}
+
+            //// in mang le
+            //Console.WriteLine("Mang le:");
+            //for (int i = 0; i < le.Length; i++)
+            //{
+            //    Console.WriteLine(le[i]);
+            //}
+
+            ////Bài 6
+
+            //// Sap xep tang dan
+            //Array.Sort(A);
+
+            //// Dao nguoc thu tu
+            //Array.Reverse(A);
+
+            //// in mang giam dan
+            //Console.WriteLine("Mang theo gia tri giam dan:");
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    Console.WriteLine(A[i]);
+            //}
+
+            //Bài 7
+
+            //int max1 = int.MinValue; // Giá trị lớn nhất
+            //int max2 = int.MinValue; // Giá trị lớn thứ hai
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    if (A[i] > max1)
+            //    {
+            //        max2 = max1;
+            //        max1 = A[i];
+            //    }
+            //    else if (A[i] > max2 && A[i] < max1)
+            //    {
+            //        max2 = A[i];
+            //    }
+            //}
+
+            //if (max2 == int.MinValue)
+            //{
+            //    Console.WriteLine("Khong ton tai gia tri lon thu hai trong mang.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Gia tri lon thu hai trong mang la: " + max2);
+            //}
+
+
+            Console.ReadKey();
         }
     }
 }
